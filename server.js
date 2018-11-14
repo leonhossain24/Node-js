c4ef9928d0b5aa13b8c76fdc1cc666862569b7b2
leonhossain <leon.hossain2427@gmail.com>
2018-11-14 03:32:25 +0000

    master

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express = require('express');
 var app = express();
 var server = http.Server(app);
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({encoded:true}));
+
+
 app.get('/', function(request, response){
     response.sendFile(__dirname+'/index.html');
     
@@ -21,6 +27,25 @@ app.get('/about', function(request, response){
 });
 
 
+ app.get('/new-article', function(request, response)
+  {
+      response.sendFile(__dirname+'/form.html');
+  });
+  
+  
+  
+  app.post('/api/article/create', function(request, response)
+  {
+      console.log(request.body);
+      if(!request.body.title){
+         
+          return response.status(400).json({error: 'PLease add a title!'});
+      }
+      response.status(200).json({message: 'Article Created!'});
+  });
+  
+
  server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
+    
   });
